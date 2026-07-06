6291f2153c7d42dea31e4f5bee967f150cadb137
Harsh Solanki <harshu6278@gmail.com>
2026-07-07 01:38:01 +0530

    adding new features

--- server/middleware/validate.js
+++ server/middleware/validate.js
@@ -1,18 +1,42 @@
 import { validationResult } from "express-validator";
 import { errorResponse } from "../utils/apiResponse.js";
 
-const validate = (req, res, next) => {
-  const errors = validationResult(req);
-  if (errors.isEmpty()) {
-    return next();
+const validate = (...args) => {
+  if (args.length === 3 && typeof args[2] === "function") {
+    const [req, res, next] = args;
+    const errors = validationResult(req);
+    if (errors.isEmpty()) {
+      return next();
+    }
+    const extractedErrors = errors.array().map((err) => ({
+      field: err.path,
+      message: err.msg,
+    }));
+    return errorResponse(res, 400, "Validation failed", extractedErrors);
   }
 
-  const extractedErrors = errors.array().map((err) => ({
-    field: err.path,
-    message: err.msg,
-  }));
+  const validations = args[0];
 
-  return errorResponse(res, 400, "Validation failed", extractedErrors);
+  return async (req, res, next) => {
+    if (Array.isArray(validations)) {
+      for (const validation of validations) {
+        const result = await validation.run(req);
+        if (result.errors && result.errors.length) break;
+      }
+    }
+
+    const errors = validationResult(req);
+    if (errors.isEmpty()) {
+      return next();
+    }
+
+    const extractedErrors = errors.array().map((err) => ({
+      field: err.path,
+      message: err.msg,
+    }));
+
+    return errorResponse(res, 400, "Validation failed", extractedErrors);
+  };
 };
 
 export default validate;
